bcf2b33d7ffa539859c82efd26d4923d6f18ad80
Florian Zeller <florian.zeller@gmail.com>
2015-05-30 15:51:27 +0200

    Correct db test: print solution

--- routes/test.js
+++ routes/test.js
@@ -1,15 +1,15 @@
 module.exports = function(ctx) {
-		ctx.app.get("/about", function(req, res) {
-				ctx.pool.getConnection(function(err, conn) {
-						if (err) {
-								res.send(err);
-								return;
-						}
-						conn.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-								var result = rows[0];
-								conn.release();
-								res.send('Result: ' + result);
-						});
-				});
+	ctx.app.get("/about", function(req, res) {
+		ctx.pool.getConnection(function(err, conn) {
+			if (err) {
+				res.send(err);
+				return;
+			}
+			conn.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+				var result = rows[0].solution;
+				conn.release();
+				res.send('Result: ' + result);
+			});
 		});
+	});
 }
