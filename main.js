63c5678f8ad6f4b7b82a573c881c0669b8d1fe3e
Florian Zeller <florian.zeller@gmail.com>
2015-05-30 17:07:38 +0200

    Orm works

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
 var express = require('express');
 var requireFu = require('require-fu');
 var mysqldb = require('./lib/mysqldb');
+var orm = require('orm');
 
 var ctx = {};
 ctx.app = express();
-ctx.pool = mysqldb.createConnectionPool();
-ctx.pool.getConnection(function(err, conn) { if (err) throw err; });
+//ctx.pool = mysqldb.createConnectionPool();
+//ctx.pool.getConnection(function(err, conn) { if (err) throw err; });
+
+var opts = {
+	database: 'sport',
+	protocol: 'mysql',
+	host: process.env.OPENSHIFT_MYSQL_DB_HOST || "127.0.0.1",
+	port: process.env.OPENSHIFT_MYSQL_DB_PORT || 3306,
+	user: 'adminm9YzrXI',
+	password: '3mfbsClKVgyw',
+	query: {
+		pool: true
+	}
+};
+
+ctx.app.use(orm.express(opts, {
+	define: function(db, models) {
+		models.post = db.define('post', {
+			title: String
+		});
+		models.post.sync(function(err) {
+			if (err) throw err;
+		});
+	}
+}));
 
 requireFu(__dirname + '/routes')(ctx);
+
+ctx.app.get('/posts', function (req, res) {
+	req.models.post.find({}, function(err, posts) {
+		if (err) throw err;
+		res.send(posts);
+	});
+});
 
 ctx.app.get('/', function (req, res) {
   res.send('Hello World!');
