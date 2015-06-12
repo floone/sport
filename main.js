df503c232b0d69da4d8049be6f68511ae7f4c9e3
Florian Zeller <florian.zeller@gmail.com>
2015-06-12 16:06:41 +0200

    Experimental approach to logging + some test data

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ var requireFu = require('require-fu');
 var ctx = {};
 ctx.app = express();
 
+ctx.error = function(s, res, t) {
+	if (res && t) {
+		res.send(t);
+	}
+	throw new Date() + ' ERROR: ' + s; 
+}
+ctx.info  = function(s) { console.log(new Date() + ' INFO: ' + s); }
+ctx.debug = function(s) { console.log(new Date() + ' DEBUG: ' + s); }
+
 requireFu(__dirname + '/routes')(ctx);
 
 ctx.app.get('/', function (req, res) {
--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,12 @@ module.exports = function(ctx) {
 			res.send(posts);
 		});
 	});
+	ctx.app.get("/posts/:eventId", function(req, res) {
+		if (isNaN(req.params.eventId)) ctx.error('eventId must be numeric', res, []);
+		req.models.post.find({ event_id: req.params.eventId }, 20, function(err, posts) {
+			if (err) throw err;
+			res.send(posts);
+		});
+	});
+	
 }
