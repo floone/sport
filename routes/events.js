7751721d3ea0712e08653d7d486edb103afaf349
Florian Zeller <florian.zeller@gmail.com>
2015-06-14 14:53:59 +0200

    Add events query and test insert

---
+++ routes/events.js
@@ -0,0 +1,31 @@
+module.exports = function(ctx) {
+	
+	var MAX_READ = 20;
+	
+	var queryEvents = function(res, model, leagueId) {
+		query = {};
+		if (isNaN(leagueId)) ctx.error('leagueId must be numeric', res, []);
+		query.league_id = leagueId;
+		model.find(query, MAX_READ, function(err, events) {
+			if (err) throw err;
+			res.send(events);
+		});
+	}
+	
+	ctx.app.get("/events/:leagueId", function(req, res) {
+		queryEvents(res, req.models.event, req.params.leagueId);
+	});
+	
+	ctx.app.post("/admin/events/insert/:leagueId/:teama/:teamb", function(req, res) {
+		ev = {};
+		ev.league_id = req.params.leagueId;
+		ev.teama = req.params.teama;
+		ev.teamb = req.params.teamb;
+		ev.datetime = new Date().toMysqlDate();
+		req.models.event.create(ev, function(err, items) {
+			if (err) throw err;
+			res.send({success: true})
+		});
+	});
+	
+}
