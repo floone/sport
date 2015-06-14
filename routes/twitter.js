2e0b7e370ab46abad44fe9d74c37c0af2b0c870a
Florian Zeller <florian.zeller@gmail.com>
2015-06-14 13:57:02 +0200

    Require twitter environment variables at startup

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,10 +1,11 @@
 module.exports = function(ctx) {
 	var OAuth = require('oauth').OAuth;
+	var msg = 'Required: TW_CONSUMER_KEY, TW_CONSUMER_SECRET, TW_ACCESS_TOKEN, TW_ACCESS_TOKEN_SECRET';
 
-	var consumerKey        = process.env.TW_CONSUMER_KEY        || 'not configured';
-	var consumerSecret     = process.env.TW_CONSUMER_SECRET     || 'not configured';
-	var accessToken        = process.env.TW_ACCESS_TOKEN        || 'not configured';
-	var accessTokenSecret  = process.env.TW_ACCESS_TOKEN_SECRET || 'not configured';
+	var consumerKey        = process.env.TW_CONSUMER_KEY        || ctx.error('Missing TW_CONSUMER_KEY. ' + msg);
+	var consumerSecret     = process.env.TW_CONSUMER_SECRET     || ctx.error('Missing TW_CONSUMER_SECRET. ' + msg);
+	var accessToken        = process.env.TW_ACCESS_TOKEN        || ctx.error('Missing TW_ACCESS_TOKEN. ' + msg);
+	var accessTokenSecret  = process.env.TW_ACCESS_TOKEN_SECRET || ctx.error('Missing TW_ACCESS_TOKEN_SECRET. ' + msg);
 	var requestTokenUrl    = process.env.TW_REQUEST_TOKEN_URL   || 'https://api.twitter.com/oauth/request_token';
 	var accessTokenUrl     = process.env.TW_ACCESS_TOKEN_URL    || 'https://api.twitter.com/oauth/access_token';
 	var oauthVersion       = process.env.TW_OAUTH_VERSION       || '1.0A';
