06c1fd839ca4dacf92fbc3218169233dfba9e500
Florian Zeller <florian.zeller@gmail.com>
2015-06-14 17:27:43 +0200

    Add /leagues route, add ability to add league by post

---
+++ routes/leagues.js
@@ -0,0 +1,26 @@
+module.exports = function(ctx) {
+	
+	var MAX_READ = 20;
+	
+	var queryEvents = function(res, model, leagueId) {
+		query = {};
+		model.find(query, MAX_READ, function(err, leagues) {
+			if (err) throw err;
+			res.send(leagues);
+		});
+	}
+	
+	ctx.app.get("/leagues", function(req, res) {
+		queryEvents(res, req.models.league, req.params.leagueId);
+	});
+	
+	ctx.app.post("/admin/leagues/insert/:leagueName", function(req, res) {
+		league = {};
+		league.league_name = req.params.leagueName;
+		req.models.league.create(league, function(err, items) {
+			if (err) throw err;
+			res.send(items)
+		});
+	});
+	
+}
