cd4396169d174cd5d92a7e2b47a0f0d37dabd33d
Florian Zeller <florian.zeller@gmail.com>
2015-07-03 17:33:30 +0200

    Add generic find method

--- routes/admin.js
+++ routes/admin.js
@@ -10,5 +10,16 @@ module.exports = function(ctx) {
 			}
 		});
 	});
-	
+
+	ctx.app.post("/admin/find/:entity", ctx.auth, function(req, res) {
+		req.models[req.params.entity].find(req.body, function(err, items) {
+			if (err) {
+				res.status(500).send(err);
+			}
+			else {
+				res.send(items);
+			}
+		});
+	});
+
 };
