faccb9e496a8a147c9bb21c821df2f36d96cdf4c
Florian Zeller <florian.zeller@gmail.com>
2015-07-03 20:15:50 +0200

    Use correct OPENSHIFT_REPO_DIR instead of OPENSHIFT_HOME

--- .openshift/cron/daily/grabevents.sh
+++ .openshift/cron/daily/grabevents.sh
@@ -1,5 +1,5 @@
 [ "$OPENSHIFT_TMP_DIR" == "" ] && echo "OPENSHIFT_TMP_DIR must be set" && exit 1
-[ "$OPENSHIFT_HOMEDIR" == "" ] && echo "OPENSHIFT_HOMEDIR must be set" && exit 1
+[ "$OPENSHIFT_REPO_DIR" == "" ] && echo "OPENSHIFT_REPO_DIR must be set" && exit 1
 [ "$GRAB_EVENTS_URL " == "" ] && echo "GRAB_EVENTS_URL must be set" && exit 1
 curl -s $GRAB_EVENTS_URL >$OPENSHIFT_TMP_DIR/fixtures.html || exit 2
-$OPENSHIFT_HOMEDIR/scripts/import-events.sh || exit 4
+$OPENSHIFT_REPO_DIR/scripts/import-events.sh || exit 4
