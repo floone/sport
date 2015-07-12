c7829cc3e596ee646b227e042261838b7618b10b
Florian Zeller <florian.zeller@gmail.com>
2015-07-12 15:05:46 +0200

    More compact if else in admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,23 +2,15 @@ module.exports = function(ctx) {
 
 	ctx.app.post("/admin/insert/:entity", ctx.auth, function(req, res) {
 		req.models[req.params.entity].create(req.body, function(err, items) {
-			if (err) {
-				res.status(500).send(err);
-			}
-			else {
-				res.send(items);	
-			}
+			if (err) res.status(500).send(err);
+			else res.send(items);
 		});
 	});
 
 	ctx.app.post("/admin/find/:entity", ctx.auth, function(req, res) {
 		req.models[req.params.entity].find(req.body, function(err, items) {
-			if (err) {
-				res.status(500).send(err);
-			}
-			else {
-				res.send(items);
-			}
+			if (err) res.status(500).send(err);
+			else res.send(items);
 		});
 	});
 
