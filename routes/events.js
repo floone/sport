ae07505b182cca0ca2351a5d4781cb51e7d15fb3
Florian Zeller <florian.zeller@gmail.com>
2015-07-13 16:21:48 +0000

    List events of latest round, add optional round parameter

--- routes/events.js
+++ routes/events.js
@@ -2,18 +2,37 @@ module.exports = function(ctx) {
 	
 	var MAX_READ = 20;
 	
-	var queryEvents = function(res, model, leagueId) {
+	var queryEvents = function(res, model, leagueId, round) {
 		query = {};
 		if (isNaN(leagueId)) ctx.error('leagueId must be numeric', res, []);
 		query.league_id = leagueId;
-		model.find(query, MAX_READ, function(err, events) {
-			if (err) throw err;
-			res.send(events);
-		});
+		
+		if (round) {
+			query.round = round;
+			model.find(query, MAX_READ, function(err, events) {
+				if (err) throw err;
+				res.send(events);
+			});
+		}
+		else {
+			model.aggregate(query).max("round").get(function(err, maxRound) {
+				if (err) throw err;
+				query.round = maxRound;
+				model.find(query, MAX_READ, function(err, events) {
+					if (err) throw err;
+					res.send(events);
+				});
+			});
+		}
+		
 	};
 	
 	ctx.app.get("/events/:leagueId", function(req, res) {
-		queryEvents(res, req.models.event, req.params.leagueId);
+		queryEvents(res, req.models.event, req.params.leagueId, null);
 	});
-
+	
+	ctx.app.get("/events/:leagueId/:round", function(req, res) {
+		queryEvents(res, req.models.event, req.params.leagueId, req.params.round);
+	});
+	
 };
