e145a13a7340b69bfbb90c2ba7ba1348972b6194
Florian Zeller <florian.zeller@gmail.com>
2015-08-02 10:41:11 +0000

    Remove console.log statements when generating index file

--- inline.sh
+++ inline.sh
@@ -1,3 +1,3 @@
 cd frontend \
-  && ../scripts/inline-resources.sh index-dev.html >index.html \
+  && ../scripts/inline-resources.sh index-dev.html |grep -v "console.log('" >index.html \
   && cd .. || exit 1
